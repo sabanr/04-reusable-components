37a225456c765c45a39bc7f0aa48d5b621a1fd13
rsaban <sabanr@gmail.com>
2025-05-04 23:29:20 -0300

    Implement incrementBy functionality in App component; update reducer and input handling

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React, { useReducer } from 'react';
 enum ActionType {
 	Increment,
 	Decrement,
+	SetIncrementBy,
 }
 
 interface Action {
@@ -12,19 +13,25 @@ interface Action {
 
 interface State {
 	count: number;
+	incrementBy: number;
 }
 
 const App: React.FC = () => {
-	const [state, dispatch] = useReducer(reducer, { count: 0 } as State);
+	const [state, dispatch] = useReducer(reducer, {
+		count: 0,
+		incrementBy: 2,
+	} as State);
 
 	function reducer(state: State, action: Action): State {
 		switch (action.type) {
 			case ActionType.Increment:
-				return { ...state, count: state.count + action.payload }; //staet + action.payload;
+				return { ...state, count: state.count + state.incrementBy }; //staet + action.payload;
 
 			case ActionType.Decrement:
-				return { ...state, count: state.count - action.payload }; //staet - action.payload;
+				return { ...state, count: state.count - state.incrementBy }; //staet - action.payload;
 
+			case ActionType.SetIncrementBy:
+				return { ...state, incrementBy: action.payload }; //staet - action.payload;
 			default:
 				throw new Error();
 		}
@@ -34,15 +41,25 @@ const App: React.FC = () => {
 		<div>
 			Use Reducer: {state.count}
 			<br />
+			<input
+				title="increment by"
+				value={state.incrementBy}
+				onChange={(e) =>
+					dispatch({
+						type: ActionType.SetIncrementBy,
+						payload: Number(e.target.value),
+					})
+				}
+			/>
 			<button
 				type="button"
-				onClick={() => dispatch({ type: ActionType.Increment, payload: 1 })}
+				onClick={() => dispatch({ type: ActionType.Increment } as Action)}
 			>
 				Increment count
 			</button>
 			<button
 				type="button"
-				onClick={() => dispatch({ type: ActionType.Decrement, payload: 1 })}
+				onClick={() => dispatch({ type: ActionType.Decrement } as Action)}
 			>
 				Decrement count
 			</button>
