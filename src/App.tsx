e18e2f7c9f63011162f17b5c68f73927c4c77629
rsaban <sabanr@gmail.com>
2025-05-04 23:17:26 -0300

    Refactor App component to use useReducer for state management; update increment/decrement functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,50 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+enum ActionType {
+	Increment,
+	Decrement,
+}
+
+interface Action {
+	type: ActionType;
+	payload: number;
+}
+
+interface State {
+	count: number;
+}
 
 const App: React.FC = () => {
-	const [count, setCount] = useState(0);
+	const [state, dispatch] = useReducer(reducer, { count: 0 } as State);
+
+	function reducer(state: State, action: Action): State {
+		switch (action.type) {
+			case ActionType.Increment:
+				return { ...state, count: state.count + action.payload }; //staet + action.payload;
+
+			case ActionType.Decrement:
+				return { ...state, count: state.count - action.payload }; //staet - action.payload;
+
+			default:
+				throw new Error();
+		}
+	}
 
 	return (
 		<div>
-			{count}
-			<button type="button" onClick={() => setCount((c) => c + 1)}>
-				Increment
+			Use Reducer: {state.count}
+			<br />
+			<button
+				type="button"
+				onClick={() => dispatch({ type: ActionType.Increment, payload: 1 })}
+			>
+				Increment count
 			</button>
-			<button type="button" onClick={() => setCount((c) => c - 1)}>
-				Decrement
+			<button
+				type="button"
+				onClick={() => dispatch({ type: ActionType.Decrement, payload: 1 })}
+			>
+				Decrement count
 			</button>
 		</div>
 	);
